b716d4c85f5ffe347aaf3b2baa30c4dc3197360e
ThomDumont <tdn.1999@gmail.com>
2018-08-31 13:12:52 -0300

    Add files via upload

---
+++ FilaCircular 221170103.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#define MAX 5
+using namespace std;
+//Thomas Dumont das Neves
+
+class Fila{
+	private:
+		int v[MAX+1];
+		int i;
+		int f;
+		int n;
+	public:
+		Fila(): i(0), f(0), n(MAX+1){
+		}
+		bool enfileira(int valor){
+			if( ( ( f+1 ) %n ) == i){
+				return false;
+			}
+
+			v[f] = valor;
+			f= (f+1)%n;
+			return true;	
+		
+		}
+
+		bool desenfileira(int* desenfileirado=NULL){
+			if(i==f)
+				return false;
+		
+			if(desenfileirado){
+				*desenfileirado = v[i];
+			}
+			i=(i+1)%n;
+			return true;
+		}
+
+
+};
+
+
+int main() {
+	Fila f;
+	f.enfileira(9);
+	f.enfileira(8);
+	f.enfileira(3);
+	f.enfileira(48);
+	f.enfileira(102);
+	//f.desenfileira();
+	int temp;
+	while(f.desenfileira(&temp)){
+		cout << temp << endl;
+	}
+	
+
+}
